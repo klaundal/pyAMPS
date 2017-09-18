24a5b99525be7afd774eedf9bb49ef177e8faa77
Karl M. Laundal <karl.laundal@ift.uib.no>
2017-09-18 13:30:32 +0200

    added sh_utils.py and plot_utils.py

---
+++ plot_utils.py
@@ -0,0 +1,200 @@
+from __future__ import division
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import griddata
+from matplotlib import rc
+
+
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
+rc('text', usetex=True)
+
+
+class Polarsubplot(object):
+    def __init__(self, ax, minlat = 60, plotgrid = True, **kwargs):
+        """ pax = Polarsubplot(axis, minlat = 50, plotgrid = True, **kwargs)
+            
+            **kwargs are the plot parameters for the grid 
+
+            this is a class which handles plotting in polar coordinates, specifically
+            an MLT/MLAT grid or similar
+
+            Example:
+            --------
+            import matplotlib.pyplot as plt
+            fig = plt.figure()
+            ax = fig.add_subplot(111)   
+            pax = Polarsubplot(ax)
+            pax.MEMBERFUNCTION()
+            plt.show()
+
+
+            where memberfunctions include:
+            plotgrid()                                   - called by __init__
+            plot(mlat, mlt, **kwargs)                    - works like plt.plot
+            write(mlat, mlt, text, **kwargs)             - works like plt.text
+            scatter(mlat, mlt, **kwargs)                 - works like plt.scatter
+            writeMLTlabels(mlat = self.minlat, **kwargs) - writes MLT at given mlat - **kwargs to plt.text
+            plotarrows(mlats, mlts, north, east)         - works like plt.arrow (accepts **kwargs too)
+            contour(mlat, mlt, f)                        - works like plt.contour
+            contourf(mlat, mlt, f)                       - works like plt.contourf
+
+        """
+        self.minlat = minlat # the lower latitude boundary of the plot
+        self.ax = ax
+        self.ax.axis('equal')
+        self.minlat = minlat
+
+        self.ax.set_xlim(-1.1, 1.1)
+        self.ax.set_ylim(-1.1, 1.1)
+        self.ax.set_axis_off()
+
+
+        if plotgrid:
+            self.plotgrid(**kwargs)
+
+    def plot(self, mlat, mlt, **kwargs):
+        """ plot curve based on mlat, mlt. Calls matplotlib.plot, so any keywords accepted by this is also accepted here """
+
+        x, y = self._mltMlatToXY(mlt, mlat)
+        return self.ax.plot(x, y, **kwargs)
+
+    def write(self, mlat, mlt, text, **kwargs):
+        """ write text on specified mlat, mlt. **kwargs go to matplotlib.pyplot.text"""
+        x, y = self._mltMlatToXY(mlt, mlat) 
+
+        self.ax.text(x, y, text, **kwargs)
+
+    def scatter(self, mlat, mlt, **kwargs):
+        """ scatterplot on the polar grid. **kwargs go to matplotlib.pyplot.scatter """
+
+        x, y = self._mltMlatToXY(mlt, mlat)
+        c = self.ax.scatter(x, y, **kwargs)
+        return c
+
+    def plotgrid(self, **kwargs):
+        """ plot mlt, mlat-grid on self.ax """
+
+        self.ax.plot([-1, 1], [0 , 0], 'k--', **kwargs)
+        self.ax.plot([0, 0], [-1, 1] , 'k--', **kwargs)
+
+        latgrid = (90 - np.r_[self.minlat:90:10])/(90. - self.minlat)
+
+        angles = np.linspace(0, 2*np.pi, 360)
+
+        for lat in latgrid:
+            self.ax.plot(lat*np.cos(angles), lat*np.sin(angles), 'k--', **kwargs)
+
+    def writeMLTlabels(self, mlat = self.minlat, degrees = False, **kwargs):
+        """ write MLT labels at given latitude (default self.minlat) 
+            if degrees is true, the longitude will be written instead of hour (with 0 at midnight)
+        """
+
+        if degrees:
+            self.write(mlat, 0,    '0$^\circ$', verticalalignment = 'top'    , horizontalalignment = 'center', **kwargs)
+            self.write(mlat, 6,   '90$^\circ$', verticalalignment = 'center' , horizontalalignment = 'left'  , **kwargs) 
+            self.write(mlat, 12, '180$^\circ$', verticalalignment = 'bottom', horizontalalignment = 'center', **kwargs)
+            self.write(mlat, 18, '-90$^\circ$', verticalalignment = 'center', horizontalalignment = 'right' , **kwargs)            
+        else:
+            self.write(mlat, 0,  '00', verticalalignment = 'top'    , horizontalalignment = 'center', **kwargs)
+            self.write(mlat, 6,  '06', verticalalignment = 'center' , horizontalalignment = 'left'  , **kwargs) 
+            self.write(mlat, 12, '12', verticalalignment = 'bottom' , horizontalalignment = 'center', **kwargs)
+            self.write(mlat, 18, '18', verticalalignment = 'center' , horizontalalignment = 'right' , **kwargs)
+
+    def featherplot(self, mlats, mlts, north, east, rotation = 0, SCALE = None, size = 10, unit = '', color = 'black', markercolor = 'black', marker = 'o', markersize = 20, **kwargs):
+        """ like plotarrows, only it's not arrows but a dot with a line pointing in the arrow direction 
+            
+            kwargs go to ax.plot
+            
+            the markers at each pin can be modified by the following keywords, that go to ax.scatter:
+            marker (default 'o')
+            markersize (defult 20 - size in points^2)
+            markercolor (default black)
+
+        """
+
+        mlts = mlts.flatten()
+        mlats = mlats.flatten()
+        north = north.flatten()
+        east = east.flatten()
+        R = np.array(([[np.cos(rotation), -np.sin(rotation)], [np.sin(rotation), np.cos(rotation)]]))
+
+        if SCALE is None:
+            scale = 1.
+        else:
+
+            if unit is not None:
+                self.ax.plot([0.9, 1], [0.95, 0.95], color = color, linestyle = '-', linewidth = 2)
+                self.ax.text(0.9, 0.95, ('%.1f ' + unit) % SCALE, horizontalalignment = 'right', verticalalignment = 'center', size = size)
+
+            scale = 0.1/SCALE
+
+        for i in range(len(mlats)):
+
+            mlt = mlts[i]
+            mlat = mlats[i]
+
+            x, y = self._mltMlatToXY(mlt, mlat)
+            dx, dy = R.dot(self._northEastToCartesian(north[i], east[i], mlt).reshape((2, 1))).flatten()
+
+            self.ax.plot([x, x + dx*scale], [y, y + dy*scale], color = color, **kwargs)
+            if markersize != 0:
+                self.ax.scatter(x, y, marker = marker, c = markercolor, s = markersize, edgecolors = markercolor)
+
+
+    def contour(self, mlat, mlt, f, **kwargs):
+        """ plot contour on grid, **kwargs are given to self.ax.contour. MLT in hours - no rotation
+        """
+
+        xea, yea = self._mltMlatToXY(mlt.flatten(), mlat.flatten())
+
+        # convert to cartesian uniform grid
+        xx, yy = np.meshgrid(np.linspace(-1, 1, 150), np.linspace(-1, 1, 150))
+        points = np.vstack( tuple((xea, yea)) ).T
+        gridf = griddata(points, f.flatten(), (xx, yy))
+
+        # ... and plot
+        return self.ax.contour(xx, yy, gridf, **kwargs)
+
+
+    def contourf(self, mlat, mlt, f, **kwargs):
+        """ plot contour on grid, **kwargs are given to self.ax.contour. MLT in hours - no rotation
+        """
+
+        xea, yea = self._mltMlatToXY(mlt.flatten(), mlat.flatten())
+
+        # convert to cartesian uniform grid
+        xx, yy = np.meshgrid(np.linspace(-1, 1, 150), np.linspace(-1, 1, 150))
+        points = np.vstack( tuple((xea, yea)) ).T
+        gridf = griddata(points, f.flatten(), (xx, yy))
+
+        # ... and plot
+        return self.ax.contourf(xx, yy, gridf, **kwargs)
+
+
+
+    def _mltMlatToXY(self, mlt, mlat):
+        r = (90. - np.abs(mlat))/(90. - self.minlat)
+        a = (mlt - 6.)/12.*np.pi
+
+        return r*np.cos(a), r*np.sin(a)
+
+    def _XYtomltMlat(self, x, y):
+        """ convert x, y to mlt, mlat, where x**2 + y**2 = 1 corresponds to self.minlat """
+        x, y = np.array(x, ndmin = 1), np.array(y, ndmin = 1) # conver to array to allow item assignment
+
+        lat = 90 - np.sqrt(x**2 + y**2)*(90. - self.minlat)
+        mlt = np.arctan2(y, x)*12/np.pi + 6
+        mlt[mlt < 0] += 24
+        mlt[mlt > 24] -= 24
+
+        return lat, mlt
+
+
+    def _northEastToCartesian(self, north, east, mlt):
+        a = (mlt - 6)/12*np.pi # convert MLT to angle with x axis (pointing from pole towards dawn)
+        
+        x1 = np.array([-north*np.cos(a), -north*np.sin(a)]) # arrow direction towards origin (northward)
+        x2 = np.array([-east*np.sin(a),  east*np.cos(a)])   # arrow direction eastward
+
+        return x1 + x2
+
---
+++ sh_utils.py
@@ -0,0 +1,225 @@
+""" tools that are useful for spherical harmonic analysis
+
+    SHkeys       -- class to contain n and m - the indices of the spherical harmonic terms
+    nterms       -- function which calculates the number of terms in a 
+                    real expansion of a poloidal (internal + external) and toroidal expansion 
+    get_legendre -- calculate associated legendre functions - with option for Schmidt semi-normalization
+"""
+import numpy as np
+d2r = np.pi/180
+
+class SHkeys(object):
+
+    def __init__(self, Nmax, Mmax):
+        """ container for n and m in spherical harmonics
+
+            keys = SHkeys(Nmax, Mmax)
+
+            keys will behave as a tuple of tuples, more or less
+            keys['n'] will return a list of the n's
+            keys['m'] will return a list of the m's
+            keys[3] will return the fourth n,m tuple
+
+            keys is also iterable
+
+        """
+
+        keys = []
+        for n in range(Nmax + 1):
+            for m in range(Mmax + 1):
+                keys.append((n, m))
+
+        self.keys = tuple(keys)
+        self.make_arrays()
+
+    def __getitem__(self, index):
+        if index == 'n':
+            return [key[0] for key in self.keys]
+        if index == 'm':
+            return [key[1] for key in self.keys]
+
+        return self.keys[index]
+
+    def __iter__(self):
+        for key in self.keys:
+            yield key
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __repr__(self):
+        return ''.join(['n, m\n'] + [str(key)[1:-1] + '\n' for key in self.keys])[:-1]
+
+    def __str__(self):
+        return ''.join(['n, m\n'] + [str(key)[1:-1] + '\n' for key in self.keys])[:-1]
+
+    def setNmin(self, nmin):
+        """ set minimum n """
+        self.keys = tuple([key for key in self.keys if key[0] >= nmin])
+        self.make_arrays()
+        return self
+
+    def MleN(self):
+        """ set m <= n """
+        self.keys = tuple([key for key in self.keys if abs(key[1]) <= key[0]])
+        self.make_arrays()
+        return self
+
+    def Mge(self, limit):
+        """ set m <= n """
+        self.keys = tuple([key for key in self.keys if abs(key[1]) >= limit])
+        self.make_arrays()
+        return self
+
+    def NminusModd(self):
+        """ remove keys if n - m is even """
+        self.keys = tuple([key for key in self.keys if (key[0] - abs(key[1])) % 2 == 1])
+        self.make_arrays()
+        return self
+
+    def NminusMeven(self):
+        """ remove keys if n - m is odd """
+        self.keys = tuple([key for key in self.keys if (key[0] - abs(key[1])) % 2 == 0])
+        self.make_arrays()
+        return self
+
+    def negative_m(self):
+        """ add negative m to the keys """
+        keys = []
+        for key in self.keys:
+            keys.append(key)
+            if key[1] != 0:
+                keys.append((key[0], -key[1]))
+        
+        self.keys = tuple(keys)
+        self.make_arrays()
+        
+        return self
+
+
+    def make_arrays(self):
+        """ prepare arrays with shape ( 1, len(keys) )
+            these are used when making G matrices
+        """
+
+        if len(self) > 0:
+            self.m = np.array(self)[:, 1][np.newaxis, :]
+            self.n = np.array(self)[:, 0][np.newaxis, :]
+        else:
+            self.m = np.array([])[np.newaxis, :]
+            self.n = np.array([])[np.newaxis, :]
+
+
+
+def nterms(NT = 0, MT = 0, NVi = 0, MVi = 0, NVe = 0, MVe = 0):
+    """ return number of coefficients in an expansion in real spherical harmonics of
+        toroidal magnetic potential truncated at NT, MT
+        poloidal magnetic potential truncated at NVi, MVi for internal sources
+        poloidal magnetic potential truncated at NVe, MVe for external sources
+    """
+
+    return len(SHkeys(NT , MT ).setNmin(1).MleN().Mge(0)) + \
+           len(SHkeys(NT , MT ).setNmin(1).MleN().Mge(1)) + \
+           len(SHkeys(NVe, MVe).setNmin(1).MleN().Mge(0)) + \
+           len(SHkeys(NVe, MVe).setNmin(1).MleN().Mge(1)) + \
+           len(SHkeys(NVi, MVi).setNmin(1).MleN().Mge(0)) + \
+           len(SHkeys(NVi, MVi).setNmin(1).MleN().Mge(1))
+
+
+
+def get_legendre(nmax, mmax, theta, schmidtnormalize = True, negative_m = False, minlat = 0, keys = None):
+    """ calculate associated Legendre functions 
+
+        nmax             -- maximum total wavenumber
+        mmax             -- maximum zonal wavenumber
+        theta            -- colatitude in degrees (not latitude!), with N terms
+        schmidtnormalize -- True if Schmidth seminormalization is wanted, False otherwise
+        negative_m       -- True if you want the functions for negative m (complex expansion)
+        keys             -- pass SHkeys object to return an array instead of a dict
+                            default None
+
+
+        returns:
+          P, dP -- dicts of legendre functions, and derivatives, with wavenumber tuple as keys
+        
+        or, if keys != None:
+          PdP   -- array of size N, 2*M, where M is the number of terms. The first half of the 
+                   columns are P, and the second half are dP
+
+
+        algorithm from "Spacecraft Attitude Determination and Control" by James Richard Wertz
+        
+        could be unstable for large nmax...
+
+        KML 2016-04-22
+
+    """
+
+    theta = theta.flatten()[:, np.newaxis]
+
+    P = {}
+    dP = {}
+    sinth = np.sin(d2r*theta)
+    costh = np.cos(d2r*theta)
+
+    if schmidtnormalize:
+        S = {}
+        S[0, 0] = 1.
+
+    # initialize the functions:
+    for n in range(nmax +1):
+        for m in range(nmax + 1):
+            P[n, m] = np.zeros_like(theta, dtype = np.float64)
+            dP[n, m] = np.zeros_like(theta, dtype = np.float64)
+
+    P[0, 0] = np.ones_like(theta, dtype = np.float64)
+    P[0, 0][np.abs(90 - theta) < minlat] = 0
+    for n in range(1, nmax +1):
+        for m in range(0, min([n + 1, mmax + 1])):
+            # do the legendre polynomials and derivatives
+            if n == m:
+                P[n, n]  = sinth * P[n - 1, m - 1]
+                dP[n, n] = sinth * dP[n - 1, m - 1] + costh * P[n - 1, n - 1]
+            else:
+
+                if n == 1:
+                    Knm = 0.
+                    P[n, m]  = costh * P[n -1, m]
+                    dP[n, m] = costh * dP[n - 1, m] - sinth * P[n - 1, m]
+
+                elif n > 1:
+                    Knm = ((n - 1)**2 - m**2) / ((2*n - 1)*(2*n - 3))
+                    P[n, m]  = costh * P[n -1, m] - Knm*P[n - 2, m]
+                    dP[n, m] = costh * dP[n - 1, m] - sinth * P[n - 1, m] - Knm * dP[n - 2, m]
+
+            if schmidtnormalize:
+                # compute Schmidt normalization
+                if m == 0:
+                    S[n, 0] = S[n - 1, 0] * (2.*n - 1)/n
+                else:
+                    S[n, m] = S[n, m - 1] * np.sqrt((n - m + 1)*(int(m == 1) + 1.)/(n + m))
+
+
+    if schmidtnormalize:
+        # now apply Schmidt normalization
+        for n in range(1, nmax + 1):
+            for m in range(0, min([n + 1, mmax + 1])):
+                P[n, m]  *= S[n, m]
+                dP[n, m] *= S[n, m]
+
+    if negative_m:
+        for n  in range(1, nmax + 1):
+            for m in range(0, min([n + 1, mmax + 1])):
+                P[n, -m]  = -1.**(-m) * factorial(n-m)/factorial(n+m) *  P[n, m]
+                dP[n, -m] = -1.**(-m) * factorial(n-m)/factorial(n+m) * dP[n, m]
+
+
+    if keys is None:
+        return P, dP
+    else:
+        Pmat  = np.hstack(tuple(P[key] for key in keys))
+        dPmat = np.hstack(tuple(dP[key] for key in keys)) 
+    
+        return np.hstack((Pmat, dPmat))
+
+
