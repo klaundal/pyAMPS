60f043f780c8fcfef7f8e955e6ca1a4f1632c7de
miktoki <mikael.toresen@gmail.com>
2017-11-10 10:12:04 +0100

    import statements

--- pyamps/__init__.py
+++ pyamps/__init__.py
@@ -1,9 +1,9 @@
-from AMPS import AMPS, get_B_ground, get_B_space
+from .amps import AMPS, get_B_ground, get_B_space
+from .mlt_utils import mlon_to_mlt
 import plot_utils
 import sh_utils
 import model_utils
-from mlt_utils import mlon_to_mlt
 
 
 
-__version__ = "1.0"
+__version__ = "1.0.0"
