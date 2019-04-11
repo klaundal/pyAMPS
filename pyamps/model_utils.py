0eb519bf26dc35758fc226acb4718826d33e47d5
Karl Laundal <karl.laundal@ift.uib.no>
2019-04-11 12:37:16 +0200

    Updated model coefficients

--- pyamps/model_utils.py
+++ pyamps/model_utils.py
@@ -5,7 +5,8 @@ from functools import reduce
 
 basepath = os.path.dirname(__file__)
 
-coeff_fn = os.path.abspath(os.path.join(basepath,'coefficients','SW_OPER_MIO_SHA_2E_00000000T000000_99999999T999999_0101.txt'))
+#coeff_fn = os.path.abspath(os.path.join(basepath,'coefficients','SW_OPER_MIO_SHA_2E_00000000T000000_99999999T999999_0101.txt'))
+coeff_fn = os.path.abspath(os.path.join(basepath,'coefficients','model_coefficients.txt'))
 
 # read coefficient file and store in pandas DataFrame - with column names from last row of header:
 names = ([x for x in open(coeff_fn).readlines() if x.startswith('#')][-1][1:]).strip().split(' ') 
