f01ee6b247eff9eed8bd2ffd859fea20a0d8c127
Karl Laundal <kalau@space.dtu.dk>
2026-05-25 19:41:33 +0200

    making model vector conversion import-safe

--- pyamps/model_vector_to_txt.py
+++ pyamps/model_vector_to_txt.py
@@ -9,15 +9,27 @@ from __future__ import absolute_import
 import time
 import numpy as np
 import pandas as pd
-from sh_utils import SHkeys
-from .coefficients import MODEL_VECTOR_LATEST, MODEL_COEFF_LATEST
 
-# header
-t = time.ctime().split(' ')
-date = ' '.join([t[1], t[-1]])
-header = '# Sherical harmonic coefficients for the Average Magnetic field and Polar current System (AMPS) model\n'
-header = header + '# Produced ' + date
-header = header + """
+try:
+    from .sh_utils import SHkeys
+    from .coefficients import MODEL_VECTOR_LATEST, MODEL_COEFF_LATEST
+except ImportError: # allow execution from inside the pyamps directory
+    from sh_utils import SHkeys
+    from coefficients import MODEL_VECTOR_LATEST, MODEL_COEFF_LATEST
+
+external_parameters = ['const', 'sinca', 'cosca', 'epsilon', 'epsilon_sinca', 'epsilon_cosca', 'tilt', 
+                       'tilt_sinca', 'tilt_cosca', 'tilt_epsilon', 'tilt_epsilon_sinca', 'tilt_epsilon_cosca', 
+                       'tau', 'tau_sinca', 'tau_cosca', 'tilt_tau', 'tilt_tau_sinca', 'tilt_tau_cosca', 'f107']
+
+NTERMS = len(external_parameters) # number of terms in the expansion of each spherical harmonic coefficient
+
+
+def make_header():
+    t = time.ctime().split(' ')
+    date = ' '.join([t[1], t[-1]])
+    header = '# Sherical harmonic coefficients for the Average Magnetic field and Polar current System (AMPS) model\n'
+    header = header + '# Produced ' + date
+    header = header + """
 #
 # Based on magnetic field measurements from CHAMP (2001-08 to 2010-09) and Swarm (2013-12 to 2023-12).
 # Reference: Laundal et al., "Solar wind and seasonal influence on ionospheric currents", Journal of Geophysical Research - Space Physics, doi:10.1029/2018JA025387, 2018
@@ -31,34 +43,28 @@ header = header + """
 # column names:
 # n m tor_c_const tor_s_const pol_c_const pol_s_const tor_c_sinca tor_s_sinca pol_c_sinca pol_s_sinca tor_c_cosca tor_s_cosca pol_c_cosca pol_s_cosca tor_c_epsilon tor_s_epsilon pol_c_epsilon pol_s_epsilon tor_c_epsilon_sinca tor_s_epsilon_sinca pol_c_epsilon_sinca pol_s_epsilon_sinca tor_c_epsilon_cosca tor_s_epsilon_cosca pol_c_epsilon_cosca pol_s_epsilon_cosca tor_c_tilt tor_s_tilt pol_c_tilt pol_s_tilt tor_c_tilt_sinca tor_s_tilt_sinca pol_c_tilt_sinca pol_s_tilt_sinca tor_c_tilt_cosca tor_s_tilt_cosca pol_c_tilt_cosca pol_s_tilt_cosca tor_c_tilt_epsilon tor_s_tilt_epsilon pol_c_tilt_epsilon pol_s_tilt_epsilon tor_c_tilt_epsilon_sinca tor_s_tilt_epsilon_sinca pol_c_tilt_epsilon_sinca pol_s_tilt_epsilon_sinca tor_c_tilt_epsilon_cosca tor_s_tilt_epsilon_cosca pol_c_tilt_epsilon_cosca pol_s_tilt_epsilon_cosca tor_c_tau tor_s_tau pol_c_tau pol_s_tau tor_c_tau_sinca tor_s_tau_sinca pol_c_tau_sinca pol_s_tau_sinca tor_c_tau_cosca tor_s_tau_cosca pol_c_tau_cosca pol_s_tau_cosca tor_c_tilt_tau tor_s_tilt_tau pol_c_tilt_tau pol_s_tilt_tau tor_c_tilt_tau_sinca tor_s_tilt_tau_sinca pol_c_tilt_tau_sinca pol_s_tilt_tau_sinca tor_c_tilt_tau_cosca tor_s_tilt_tau_cosca pol_c_tilt_tau_cosca pol_s_tilt_tau_cosca tor_c_f107 tor_s_f107 pol_c_f107 pol_s_f107
 """
+    return header
 
 
-# load model vector and define truncation levels and external parametrisation
-model_vector = np.load(MODEL_VECTOR_LATEST)
-NT, MT, NV, MV = 65, 3, 45, 3
+def make_keys(NT = 65, MT = 3, NV = 45, MV = 3):
+    """ make spherical harmonic keys """
 
-external_parameters = ['const', 'sinca', 'cosca', 'epsilon', 'epsilon_sinca', 'epsilon_cosca', 'tilt', 
-                       'tilt_sinca', 'tilt_cosca', 'tilt_epsilon', 'tilt_epsilon_sinca', 'tilt_epsilon_cosca', 
-                       'tau', 'tau_sinca', 'tau_cosca', 'tilt_tau', 'tilt_tau_sinca', 'tilt_tau_cosca', 'f107']
+    keys = {}
+    keys['cos_T'] = SHkeys(NT, MT).setNmin(1).MleN().Mge(0)
+    keys['sin_T'] = SHkeys(NT, MT).setNmin(1).MleN().Mge(1)
+    keys['cos_V'] = SHkeys(NV, MV).setNmin(1).MleN().Mge(0)
+    keys['sin_V'] = SHkeys(NV, MV).setNmin(1).MleN().Mge(1)
 
-NTERMS = len(external_parameters) # number of terms in the expansion of each spherical harmonic coefficient
-
-
-""" make spherical harmonic keys """
-keys = {}
-keys['cos_T'] = SHkeys(NT, MT).setNmin(1).MleN().Mge(0)
-keys['sin_T'] = SHkeys(NT, MT).setNmin(1).MleN().Mge(1)
-keys['cos_V'] = SHkeys(NV, MV).setNmin(1).MleN().Mge(0)
-keys['sin_V'] = SHkeys(NV, MV).setNmin(1).MleN().Mge(1)
-
-m_cos_V = keys['cos_V'].m
-m_sin_V = keys['sin_V'].m
-m_cos_T = keys['cos_T'].m
-m_sin_T = keys['sin_T'].m
+    return keys
 
 
-def vector_to_df(m_vec):
+def vector_to_df(m_vec, keys):
     """ convert model vector to dataframes, one for each coefficient (cos and sin for T and V). The index is (n,m) """
+
+    m_cos_V = keys['cos_V'].m
+    m_sin_V = keys['sin_V'].m
+    m_cos_T = keys['cos_T'].m
+    m_sin_T = keys['sin_T'].m
 
     tor_c = pd.Series(m_vec[                                           : m_cos_T.size                              ], index = keys['cos_T'], name = 'tor_c')
     tor_s = pd.Series(m_vec[m_cos_T.size                               : m_cos_T.size + m_sin_T.size               ], index = keys['sin_T'], name = 'tor_s')
@@ -68,22 +74,35 @@ def vector_to_df(m_vec):
     # merge the series into one DataFrame, and fill in zeros where the terms are undefined
     return pd.concat((tor_c, tor_s, pol_c, pol_s), axis = 1)
 
-# get one set of coefficients per external parameter, and store in dataframes where columns are 'tor_c', 'tor_s', 'pol_c', and 'pol_s'
-# _c and _s refer to cos and sin terms, respectively, and tor and pol to toroidal and poloidal
-dataframes = [vector_to_df(m) for m in np.split(model_vector, NTERMS)]
 
-# add the external parameter to the column names:
-for m, param in zip(dataframes, external_parameters):
-    m.columns = [n + '_' + param for n in m.columns]
+def model_vector_to_coefficients(model_vector, NT = 65, MT = 3, NV = 45, MV = 3):
+    keys = make_keys(NT = NT, MT = MT, NV = NV, MV = MV)
 
-# merge them all
-coefficients = pd.concat(dataframes, axis = 1)
+    # get one set of coefficients per external parameter, and store in dataframes where columns are 'tor_c', 'tor_s', 'pol_c', and 'pol_s'
+    # _c and _s refer to cos and sin terms, respectively, and tor and pol to toroidal and poloidal
+    dataframes = [vector_to_df(m, keys) for m in np.split(model_vector, NTERMS)]
 
-# write txt file
-with open(MODEL_COEFF_LATEST, 'w') as file:
-    # header:
-    file.write(header)
-    # data:
-    coefficients.to_string(buf = file, float_format = lambda x: '{:.7f}'.format(x), 
-                           header = False, sparsify = False,
-                           index_names = False)
+    # add the external parameter to the column names:
+    for m, param in zip(dataframes, external_parameters):
+        m.columns = [n + '_' + param for n in m.columns]
+
+    # merge them all
+    return pd.concat(dataframes, axis = 1)
+
+
+def main(model_vector_fn = MODEL_VECTOR_LATEST, coeff_fn = MODEL_COEFF_LATEST):
+    model_vector = np.load(model_vector_fn)
+    coefficients = model_vector_to_coefficients(model_vector)
+
+    # write txt file
+    with open(coeff_fn, 'w') as file:
+        # header:
+        file.write(make_header())
+        # data:
+        coefficients.to_string(buf = file, float_format = lambda x: '{:.7f}'.format(x), 
+                               header = False, sparsify = False,
+                               index_names = False)
+
+
+if __name__ == "__main__":
+    main()
